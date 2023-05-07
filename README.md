10bc747cf796d974ff76ea00b6a7e8858d5069fc
nmatai <nmatai@usc.edu>
2023-05-06 22:19:37 -0700

    character card done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # supplyframe-task
+
+using - comic-vine api to get the characters and comics
+
+https://www.digitalocean.com/community/tutorials/how-to-use-ejs-to-template-your-node-application
---
+++ backend/routes/routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+
+// index page
+router.get('/', function(req, res) {
+    var mascots = [
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+      { name: 'Tux', organization: "Linux", birth_year: 1996},
+      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+  
+    res.render('pages/index', {
+      mascots: mascots,
+      tagline: tagline
+    });
+  });
+  
+  // about page
+  router.get('/about', function(req, res) {
+    res.render('pages/about');
+  });
+
+    // about page
+    router.get('/test', function(req, res) {
+        res.render('pages/character-test');
+      });
+
+module.exports = router;
