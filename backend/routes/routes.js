9661c26db7dd8c8c907f87f68477b386cbc526ec
nmatai <nmatai@usc.edu>
2023-05-07 21:22:12 -0700

    added pagination

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -22,17 +22,19 @@ router.get("/test", function (req, res) {
 
 router.get("/characters", async function (req, res) {
 
+    var page = req.query.page || 1;
+    // console.log(page);
     const query_params = {
         api_key: process.env.COMIC_VINE_API_KEY,
         field_list: "name,image,id,origin,publisher",
-        limit:10,
+        limit:12,
         format:"json",
-        offset:20
+        offset:(page-1)*12
     }
     const characters_api = await axios.get(process.env.GET_CHARACTERS_API,{params:query_params})
-    console.log(characters_api.data.results);
+    // console.log(characters_api.data.results);
 
-  res.render("pages/characters",{data:characters_api.data.results});
+  res.render("pages/characters",{data:characters_api.data.results,page:parseInt(page)});
 });
 router.get("/comics", function (req, res) {
     res.render("pages/comics");
