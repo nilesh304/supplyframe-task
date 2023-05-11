131fcbefc2718578571bd71c916f2d4dac45067b
nmatai <nmatai@usc.edu>
2023-05-10 19:25:31 -0700

    comics page made nav_page partial to be usable for both comic and characters page

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -23,21 +23,41 @@ router.get("/test", function (req, res) {
 router.get("/characters", async function (req, res) {
 
     var page = req.query.page || 1;
+    var name = req.query.name || "";
     // console.log(page);
     const query_params = {
         api_key: process.env.COMIC_VINE_API_KEY,
         field_list: "name,image,id,origin,publisher",
         limit:12,
         format:"json",
-        offset:(page-1)*12
+        offset:(page-1)*12,
+        filter:"name:"+name
     }
+    // console.log("name:"+name);
     const characters_api = await axios.get(process.env.GET_CHARACTERS_API,{params:query_params})
     // console.log(characters_api.data.results);
 
-  res.render("pages/characters",{data:characters_api.data.results,page:parseInt(page)});
+  res.render("pages/characters",{data:characters_api.data.results,page:parseInt(page),name:name});
 });
-router.get("/comics", function (req, res) {
-    res.render("pages/comics");
-  });
+
+router.get("/comics", async function (req, res) {
+
+  var page = req.query.page || 1;
+  var name = req.query.name || "";
+  // console.log(page);
+  const query_params = {
+      api_key: process.env.COMIC_VINE_API_KEY,
+      field_list: "cover_date,image",
+      limit:12,
+      format:"json",
+      offset:(page-1)*12,
+      filter:"name:"+name
+  }
+  // console.log("name:"+name);
+  const characters_api = await axios.get(process.env.GET_COMICS_API,{params:query_params})
+  // console.log(characters_api.data.results);
+
+res.render("pages/comics",{data:characters_api.data.results,page:parseInt(page),name:name});
+});
 
 module.exports = router;
